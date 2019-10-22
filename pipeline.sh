c8b8e9940df43690b514deb0a017efae7f4eb569
Willi Carlsen <carlsenwilli@gmail.com>
2019-10-22 14:17:46 +0200

    Added pipeline script that fails when zero updates are detected

---
+++ pipeline.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Get images version
+BASE_IMAGE_NAME="bloomberg/goldpinger"
+DOCKERHUB_IMAGE=$(curl https://registry.hub.docker.com/v2/repositories/bloomberg/goldpinger/tags/\?page\=$i 2>/dev/null|jq '."results"[]["name"]' | grep -v "vendor" | head -n 1 | tr -d '"')
+NEWEST_IMAGE=$BASE_IMAGE_NAME:$DOCKERHUB_IMAGE
+CURRENT_IMAGE=$(kubectl get daemonsets goldpinger -n monitoring -o json | jq '.spec.template.spec.containers[].image' | tr -d '"')
+
+# Get helm chart versions
+CURRENT_CHART=$(helm list goldpinger --output json | jq '.Releases[].Chart' | tr -d '"')
+BASE_CHART_NAME="goldpinger"
+HELM_CHART=$(helm search goldpinger --output json | jq '.[].Version' | tr -d '"')
+NEWEST_CHART=$BASE_CHART_NAME-$HELM_CHART
+
+if [ $NEWEST_IMAGE != $CURRENT_IMAGE ] && [ $NEWEST_CHART != $CURRENT_CHART ]; then
+	echo "New image version detected updating from $CURRENT_IMAGE -> $NEWEST_IMAGE"
+	echo "New chart version detected updating from $CURRENT_CHART -> $NEWEST_CHART"
+elif [ $NEWEST_IMAGE != $CURRENT_IMAGE ]; then
+	echo "New image version detected updating from $CURRENT_IMAGE -> $NEWEST_IMAGE"
+elif [ $NEWEST_CHART != $CURRENT_CHART ]; then
+	echo "New chart version detected updating from $CURRENT_CHART -> $NEWEST_CHART"
+else
+	exit 1
+fi
